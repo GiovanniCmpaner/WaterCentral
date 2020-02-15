c50df41024c749a27c2bd460f4249b74589938ad
Giovanni Campaner <giocampaner@gmail.com>
2020-02-15 12:00:09 -0300

    - Commit inicial

---
+++ src/Peripherals.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <Arduino.h>
+
+namespace Peripherals 
+{
+    namespace SD_CARD 
+    {
+        enum Pins {
+            SS = 15,
+            MOSI = 13,
+            MISO = 12,
+            SCK = 14
+        };
+    };
+    namespace BME280 
+    {
+        enum Pins 
+        {
+            SDA = 21,
+            SCL = 22
+        }
+    };
+    namespace PAM8403 
+    {
+        enum Pins 
+        {
+            RIN = 25
+        }
+    };
+    namespace DS3231 
+    {
+        enum Pins {
+            INT = 34,
+            SCL = 22,
+            SDA = 21
+        }
+    };
+    namespace MPX_DP
+    {
+        enum Pins 
+        {
+            VOUT_1 = 33,
+            VOUT_2 = 35,
+            VOUT_3 = 32
+        }
+    }
+    enum Pins 
+    {
+        BTN = 4,
+        LED_HTB = 6,
+        PRF_CTL = 27
+    };
+};
